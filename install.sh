91749f00fa57b396efe52b529444cb1e7ffb6326
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2017-07-17 00:30:47 +0300

    Download the portmap plugin and use overlay2 as the storage driver for docker

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ apt-get update && apt-get install -y docker.io kubeadm ceph-common
 
 # Install helm
 curl -sSL https://storage.googleapis.com/kubernetes-helm/helm-v2.5.0-linux-amd64.tar.gz | tar -xz -C /usr/local/bin linux-amd64/helm --strip-components=1
+curl -sSL https://github.com/containernetworking/plugins/releases/download/v0.6.0-rc1/cni-plugins-amd64-v0.6.0-rc1.tgz | tar -xz -C /opt/cni/bin ./portmap
+
+sed -e "s|/usr/bin/dockerd|/usr/bin/dockerd -s overlay2|g" -i /lib/systemd/system/docker.service
+systemctl daemon-reload
+rm -rf /var/lib/docker
+systemctl restart docker
